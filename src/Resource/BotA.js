e636e13705bc0b9135be2b7d3de382833fded5a4
Rudrabhoj Bhati <harshvardhan.rawlot@gmail.com>
2018-06-28 03:32:17 +0530

    Added dummy files

---
+++ src/Resource/BotA.js
@@ -0,0 +1,11 @@
+class BotA {
+  constructor() {
+
+  }
+
+  say() {
+    console.log("BotA!!");
+  }
+}
+
+export default BotA;
---
+++ src/Resource/BotB.js
@@ -0,0 +1,11 @@
+class BotB {
+  constructor() {
+
+  }
+
+  yell() {
+    console.log("Why do you want me to yell BotB?");
+  }
+}
+
+export default BotB;
